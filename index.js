ac4294c6e4838f34780a158e0b57382b7bc81d8f
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-02-27 07:50:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ import routes from "./routes/routes.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", // Allow requests from your Vite frontend
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
+
 
 app.use("/", routes);
 
