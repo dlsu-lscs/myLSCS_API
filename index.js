e08357cd737a6f61d0b218b1c51fb9c18a2f1a78
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-02-26 19:43:15 +0800

    fix: changed to js

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
