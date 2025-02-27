501bd911611eeb9a496565c31a6f3f10e53b49ae
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-02-27 08:10:21 +0800

    fix:

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import routes from "./routes/routes.js";
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import routes from './routes/routes.js'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173", // Allow requests from your Vite frontend
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Content-Type,Authorization"
-}));
+app.use(express.json())
+app.use(cors())
 
+app.use('/', routes)
 
-app.use("/", routes);
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World')
+})
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
-});
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`Server running on port ${port}`)
+})
